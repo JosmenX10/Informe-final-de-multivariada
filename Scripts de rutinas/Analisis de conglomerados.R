ec50cfde21c2f7a6dfb01515e904926d7b5ea3f1
JosmenX10 <smjosuemendozah2017@gmail.com>
2024-08-21 13:27:48 -0500

    Je

--- Scripts de rutinas/Analisis de conglomerados.R
+++ Scripts de rutinas/Analisis de conglomerados.R
@@ -16,15 +16,12 @@ source("hcoplot.R")
 
 #Cargamos la base de datos de vegetación.
 
-datos <- read_xlsx("C:\\Users\\Asus\\Desktop\\Analisis multivariado\\Informe-final-de-multivariada\\Base de datos y contextos\\vltava.xlsx",
-                   sheet = "Vltava-species")
+datos_env <- read.delim("https://raw.githubusercontent.com/JosmenX10/Informe-final-de-multivariada/main/Base%20de%20datos%20y%20contextos/vltava_env.csv,sep"= ";")
 
-#Se transpone la base de datos dejando las parcelas como filas
-datos_bio <- datos[,-1]
+datos_bio <- read.delim("https://raw.githubusercontent.com/JosmenX10/Informe-final-de-multivariada/main/Base%20de%20datos%20y%20contextos/datos_bio.csv", sep =",")
 
-datos_bio <- t(datos_bio)
+datos_bio <- datos_bio[,-1]
 
-colnames(datos_bio) <- datos$...1
 #------------------------------
 
 datos_trans <- log1p(datos_bio)
@@ -35,11 +32,11 @@ dist_bio_sqrt <- sqrt(dist_bio)
 Cluster1 <- hclust(dist_bio_sqrt,method = "ward.D2")
 plot(Cluster1, hang = -1)
 
-clus_ward_cut <- cutree (Cluster1, k = 4) # "cut the tree" - to which groups individual samples belong?
+clus_ward_cut <- cutree (Cluster1, k = 5) # "cut the tree" - to which groups individual samples belong?
 
-plot (clus_ward, cex = .5)  # argument cex reduced the size of the dendrogram leaf labels to make them readable
-clus_in_dendro <- unique (clus_ward_cut[clus_ward$order]) # make sure to know which box is which cluster!
-rect.hclust (clus_ward, k = 4, border = clus_in_dendro)
+plot (Cluster1, cex = .5)  # argument cex reduced the size of the dendrogram leaf labels to make them readable
+clus_in_dendro <- unique(clus_ward_cut[Cluster1$order]) # make sure to know which box is which cluster!
+rect.hclust (Cluster1, k = 5, border = clus_in_dendro)
 legend ('topleft', legend = paste ('Cluster', 1:5), col = 1:5, pch = 22, bty = 'n')
 
 
